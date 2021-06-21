2f69a0c274b7f71339b0444238015f021fb382a7
keisuke-nao-baseball <matsukei7539@gmail.com>
2021-06-21 18:48:43 +0900

    開始

---
+++ hofuri/src/main/java/com/kei/hofuri/HofuriApplication.java
@@ -0,0 +1,13 @@
+package com.kei.hofuri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HofuriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HofuriApplication.class, args);
+	}
+
+}
---
+++ hofuri/src/test/java/com/kei/hofuri/HofuriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kei.hofuri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HofuriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
